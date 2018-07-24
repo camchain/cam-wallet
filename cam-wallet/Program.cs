7d79f61411950f06c955b1cf6b4fa7cffc90011e
zerowood <1528619192@qq.com>
2018-07-24 14:03:09 +0800

    增加根证书的安装

--- cam-wallet/Program.cs
+++ cam-wallet/Program.cs
@@ -30,92 +30,78 @@ namespace Cam
                 PrintErrorLogs(w, (Exception)e.ExceptionObject);
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        /// <summary>
+        /// 安装证书服务器的根证书
+        /// </summary>
+        /// <returns></returns>
+        private static bool InstallCertificate()
+        {
+            if (!Settings.Default.InstallCertificate) return true;
+            using (X509Store store = new X509Store(StoreName.Root, StoreLocation.LocalMachine))
+            using (X509Certificate2 cert = new X509Certificate2(Resources.CamCertificate))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                if (store.Certificates.Contains(cert)) return true;
+            }
+            using (X509Store store = new X509Store(StoreName.Root, StoreLocation.LocalMachine))
+            using (X509Certificate2 cert = new X509Certificate2(Resources.CamCertificate))
+            {
+                try
+                {
+                    store.Open(OpenFlags.ReadWrite);
+                    store.Add(cert);
+                    return true;
+                }
+                catch (CryptographicException) { }
+                if (MessageBox.Show(Strings.InstallCertificateText, Strings.InstallCertificateCaption, MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) != DialogResult.Yes)
+                {
+                    Settings.Default.InstallCertificate = false;
+                    Settings.Default.Save();
+                    return true;
+                }
+                try
+                {
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = Application.ExecutablePath,
+                        UseShellExecute = true,
+                        Verb = "runas",
+                        WorkingDirectory = Environment.CurrentDirectory
+                    });
+                    return false;
+                }
+                catch (Win32Exception) { }
+                MessageBox.Show(Strings.InstallCertificateCancel);
+                return true;
+            }
+        }        
 
         [STAThread]
         public static void Main()
-        {
-
-
-
+        {         
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            XDocument xdoc = null;
-            try
-            {
-                
-            }
-            catch { }
-            if (xdoc != null)
-            {
-                Version version = Assembly.GetExecutingAssembly().GetName().Version;
-                Version minimum = Version.Parse(xdoc.Element("update").Attribute("minimum").Value);
-                if (version < minimum)
-                {
-                    using (UpdateDialog dialog = new UpdateDialog(xdoc))
-                    {
-                        dialog.ShowDialog();
-                    }
-                    return;
-                }
-            }
-
+           
+            //判断是否安装了根证书
+            if (!InstallCertificate()) return;
+            
             const string PeerStatePath = "peers.dat";
             if (File.Exists(PeerStatePath))
+            {
                 using (FileStream fs = new FileStream(PeerStatePath, FileMode.Open, FileAccess.Read, FileShare.Read))
                 {
                     LocalNode.LoadState(fs);
                 }
+            }
 
-            using (Blockchain.RegisterBlockchain(new LevelDBBlockchain(Settings.Default.Paths.Chain)))
-
-                
+            using (Blockchain.RegisterBlockchain(new LevelDBBlockchain(Settings.Default.Paths.Chain)))                
             using (LocalNode = new LocalNode())
             {
                 LocalNode.UpnpEnabled = true;
-                Application.Run(MainForm = new MainForm(xdoc));
+                Application.Run(MainForm = new MainForm());
             }
+
             using (FileStream fs = new FileStream(PeerStatePath, FileMode.Create, FileAccess.Write, FileShare.None))
             {
                 LocalNode.SaveState(fs);
