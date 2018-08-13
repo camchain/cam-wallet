69a1ca94c2f6c67453beb176b686cf7b5842c6c0
zerowood <wujinlang2010@163.com>
2018-08-13 14:30:04 +0800

    查看私钥界面优化

--- cam-wallet/UI/ViewPrivateKeyDialog.designer.cs
+++ cam-wallet/UI/ViewPrivateKeyDialog.designer.cs
@@ -2,14 +2,15 @@
 {
     partial class ViewPrivateKeyDialog
     {
-
-
-
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-
-
-
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,9 +22,10 @@
 
         #region Windows Form Designer generated code
 
-
-
-
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ViewPrivateKeyDialog));
@@ -39,20 +41,20 @@
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-
-
-
+            // 
+            // label1
+            // 
             resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-
-
-
+            // 
+            // textBox1
+            // 
             resources.ApplyResources(this.textBox1, "textBox1");
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-
-
-
+            // 
+            // groupBox1
+            // 
             resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Controls.Add(this.textBox2);
             this.groupBox1.Controls.Add(this.label4);
@@ -60,51 +62,51 @@
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.TabStop = false;
-
-
-
+            // 
+            // textBox2
+            // 
             resources.ApplyResources(this.textBox2, "textBox2");
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-
-
-
+            // 
+            // label4
+            // 
             resources.ApplyResources(this.label4, "label4");
             this.label4.Name = "label4";
-
-
-
+            // 
+            // label3
+            // 
             resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-
-
-
+            // 
+            // button1
+            // 
             resources.ApplyResources(this.button1, "button1");
             this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.button1.Name = "button1";
             this.button1.UseVisualStyleBackColor = true;
-
-
-
+            // 
+            // textBox3
+            // 
             resources.ApplyResources(this.textBox3, "textBox3");
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
-
-
-
+            // 
+            // label2
+            // 
             resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-
-
-
+            // 
+            // textBox4
+            // 
             resources.ApplyResources(this.textBox4, "textBox4");
             this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox4.Name = "textBox4";
             this.textBox4.ReadOnly = true;
-
-
-
+            // 
+            // ViewPrivateKeyDialog
+            // 
             this.AcceptButton = this.button1;
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
