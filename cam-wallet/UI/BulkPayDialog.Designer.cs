6cc19a895d6ffc33f297c7f347a2e11a44c597af
zerowood <wujinlang2010@163.com>
2018-09-10 15:59:44 +0800

    更新

--- cam-wallet/UI/BulkPayDialog.Designer.cs
+++ cam-wallet/UI/BulkPayDialog.Designer.cs
@@ -2,14 +2,15 @@
 {
     partial class BulkPayDialog
     {
-
-
-
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-
-
-
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,9 +22,10 @@
 
         #region Windows Form Designer generated code
 
-
-
-
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BulkPayDialog));
@@ -36,54 +38,54 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
-
-
-
+            // 
+            // textBox3
+            // 
             resources.ApplyResources(this.textBox3, "textBox3");
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
-
-
-
+            // 
+            // label4
+            // 
             resources.ApplyResources(this.label4, "label4");
             this.label4.Name = "label4";
-
-
-
+            // 
+            // comboBox1
+            // 
             resources.ApplyResources(this.comboBox1, "comboBox1");
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-
-
-
+            // 
+            // label3
+            // 
             resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-
-
-
+            // 
+            // button1
+            // 
             resources.ApplyResources(this.button1, "button1");
             this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.button1.Name = "button1";
             this.button1.UseVisualStyleBackColor = true;
-
-
-
+            // 
+            // groupBox1
+            // 
             resources.ApplyResources(this.groupBox1, "groupBox1");
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.TabStop = false;
-
-
-
+            // 
+            // textBox1
+            // 
             this.textBox1.AcceptsReturn = true;
             resources.ApplyResources(this.textBox1, "textBox1");
             this.textBox1.Name = "textBox1";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-
-
-
+            // 
+            // BulkPayDialog
+            // 
             resources.ApplyResources(this, "$this");
             this.AcceptButton = this.button1;
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
